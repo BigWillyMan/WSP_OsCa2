e1168d8d1feabbba9ee13d0549708917efdf88f4
BigWillyMan <oscar.calner@tisuddevalla.se>
2018-05-03 11:09:14 +0200

    Ny

--- Laboration_PHP/PDO/resources/includes/model.php
+++ Laboration_PHP/PDO/resources/includes/model.php
@@ -7,57 +7,33 @@ $password = 'Kaka123';
 $attr = array(PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC);
 
 
-$pdo = new PDO("mysql:dbname=$dbname;host=$host", $user, $password, $attr);
+$dsn = "mysql:host=$host;dbname=$dbname;charset=utf8mb4";
 
 
-if($pdo) {
-echo "Database connected!";
+$pdo = new PDO($dsn, $user, $password, $attr);
 
-echo "<pre>";
-foreach($pdo->query('SELECT * FROM Posts') as $row) {
-print_r($row); }
-echo "</pre>";
+$sql= 'SELECT p.ID, p.Slug, p.Headline, CONCAT(u.) AS Name, p.Creation_time, p.Text FROM Posts JOIN Users ON U.ID = P.Users_ID';
+
+if ($pdo) {
+  $model = array();
+
+
+  foreach($pdo->query('SELECT * FROM Posts') as $row) {
+  $model += Array(
+      $row['ID'] => array(
+        'p.slug' => $row['p.Slug'],
+        'p.Headline' => $row['p.Headline'],
+        'p.author' => $row['p.Name'],
+        'date' => $row['p.Creation_time'],
+        'p.text' => $row['p.Text']
+        )
+
+      );
+
+    }
 
 } else {
-print_r($pdo->errorInfo());
+print_r($pdo->errorinfo());
 }
-
-
-$model = array(
-    '0' => array(
-        'slug' => 'forsta-inlagget',
-        'title' => 'Första inlägget',
-        'author' => 'Gugge',
-        'date' => '2015-01-01',
-        'text' => 'Här kommer det första inlägget i sin helhet. Välkommen till Labb 3! Här övar vi på PHP för att bli duktiga webbserverprogrammerare. Detta är tredje labben och andra labben i en serie labbar som tillsammans bygger ihop detta projekt. Ett enkelt PHP-projekt men väl strukturerat och genomtänkt konstruerat.'
-    ),
-    '1' => array(
-        'slug' => 'snart-ar-det-var',
-        'title' => 'Snart är det vår',
-        'author' => 'Gugge',
-        'date' => '2015-02-24',
-        'text' => 'Snart är det vår! Då övar vi på PHP för att bli duktiga webbserverprogrammerare. Detta är tredje labben och andra labben i en serie labbar som tillsammans bygger ihop detta projekt. Ett enkelt PHP-projekt men väl strukturerat och genomtänkt konstruerat.'
-    ),
-    '2' => array(
-        'slug' => 'robin-presenterar-sig',
-        'title' => 'Robin presenterar sig',
-        'author' => 'Robin',
-        'date' => '2015-02-25',
-        'text' => 'Robin är en trevlig prick som gärna övar på PHP för att som du bli en duktig webbserverprogrammerare. Detta är tredje labben och andra labben i en serie labbar som tillsammans bygger ihop detta projekt. Ett enkelt PHP-projekt men väl strukturerat och genomtänkt konstruerat.'
-    ),
-    '3' => array(
-        'slug' => 'senaste-inlagget',
-        'title' => 'Senaste inlägget',
-        'author' => 'Robin',
-        'date' => '2015-02-26',
-        'text' => 'Här kommer senaste inlägget i sin helhet. Välkommen till Labb 3! Här övar vi på PHP för att bli duktiga webbserverprogrammerare. Detta är tredje labben och andra labben i en serie labbar som tillsammans bygger ihop detta projekt. Ett enkelt PHP-projekt men väl strukturerat och genomtänkt konstruerat.'
-    )
-);
-
-echo "<pre>";
-foreach($model as $row) {
-print_r($row);
-}
-echo "</pre>";
 
 ?>
